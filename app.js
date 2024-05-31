a67864bda3fbcd13f036628c88741d2f54b125d8
favourabang <favourabang223@gmail.com>
2024-05-31 17:38:27 +0100

    final

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.FAVOURABANG VUG/CSC/21/5740 '));
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.FAVOURABANG VUG/CSC/21/5740 '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
